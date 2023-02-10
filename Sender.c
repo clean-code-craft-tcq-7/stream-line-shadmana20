231787c7375596bbe905d6fe903041bebda0dc0b
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-02-10 22:15:24 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "Sender.h"
 
+
+int DataGenerator(int size)
+{
+  int gen;
+  for(int i=1;i<=size;i++)
+  {
+    gen = rand()%50;
+  }
+  return gen;
+}

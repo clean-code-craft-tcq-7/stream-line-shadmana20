dd766ec4f041e8b3dc73319065a6e83e410a83fc
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-02-13 18:32:26 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -10,11 +10,13 @@
 SensorData_tst Init()
 {
   static SensorData_tst sen[SIZE];
-  SensorData_tst SensorData_st
+  SensorData_tst SensorData_st;
   for(int i=0;i<SIZE;i++)
     {
        SensorData_st->BatteryTemp[i] = rand()%50;
        SensorData_st->SensorValue[i] = rand()%50;
+       sen->BatteryTemp[i] = SensorData_st->BatteryTemp[i];
+       sen->SensorValue[i] =  SensorData_st->SensorValue[i];
     }
     return sen;
 }

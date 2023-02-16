4aa19e3a64c692c5ece53d1038d82fca62980212
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-02-16 20:50:28 +0530

    Update Sender.h

--- Sender.h
+++ Sender.h
@@ -1,7 +1,21 @@
-#include <stdio.h>
-#define SIZE  50
-typedef struct SensorData_tst SensorData_tst;
- struct SensorData_tst{
-   int BatteryTemp[SIZE];
-   int SensorValue[SIZE];
-};
+#define MAX_VALUE 100
+#define SOC_RANGE 100
+#define TEMP_RANGE 100
+
+typedef struct
+{
+  float Temprature[MAX_VALUE];
+  float Soc[MAX_VALUE];
+}SensorData_st;
+
+typedef enum
+{
+  DATA_SENDING_FAILED = 0,
+  DATA_GENERATION_FAILED,
+  DATA_GENERATION_OK,
+  DATA_SENDING_OK
+}DataStatus_en;
+
+DataStatus_en SendBatteryParameters(SensorData_st *SensorData ,int);
+DataStatus_en GenerateSensorData(SensorData_st *SensorData ,int);
+void PrintBatteryData(float,float);
